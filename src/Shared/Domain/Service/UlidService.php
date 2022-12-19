78e5784f36a1469961d10b53c571ca761426ce88
musict <infomailpl@gmail.com>
2022-12-19 18:29:34 +0100

    add doctrine

---
+++ src/Shared/Domain/Service/UlidService.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Shared\Domain\Service;
+
+
+use Symfony\Component\Uid\Ulid;
+
+class UlidService
+{
+    public static function generate(): string
+    {
+        return Ulid::generate();
+    }
+}
